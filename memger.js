a136c9e24ea131d2b3ca08c05e3bb967337bb6df
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2024-06-15 06:10:29 +0000

    Copilot second commit

---
+++ memger.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var skill = "JavaScript";
+    if (true) {
+        var skill = "Angular";
+        console.log(`Skill: ${skill}`);
+    }
+    console.log(`Skill: ${skill}`);
+}
